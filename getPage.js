43ff0a80b25f49884bbdfeeb0fcd33b76174ae58
Vladislav <lenferdetroud@gmail.com>
2022-07-03 23:12:15 +0700

    Migrating to Manifest V3

--- getPage.js
+++ getPage.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function() {
+chrome.action.onClicked.addListener(function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var tab = tabs[0];
         
